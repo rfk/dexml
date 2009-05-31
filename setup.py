b831061c47ea546393c9e1858eac35dfd3615e92
Ryan Kelly <ryan@rfk.id.au>
2009-05-31 14:17:59 +1000

    basic setup.py file

---
+++ setup.py
@@ -0,0 +1,33 @@
+#
+#  This is the dexml setuptools script.
+#  Originally developed by Ryan Kelly, 2009.
+#
+#  This script is placed in the public domain.
+#
+
+from distutils.core import setup
+
+import dexml
+VERSION = dexml.__version__
+
+NAME = "dexml"
+DESCRIPTION = "a dead-simple Object-XML mapper for Python"
+LONG_DESC = dexml.__doc__
+AUTHOR = "Ryan Kelly"
+AUTHOR_EMAIL = "ryan@rfk.id.au"
+URL="http://www.rfk.id.au/software/"
+LICENSE = "BSD"
+KEYWORDS = "windows registry"
+
+setup(name=NAME,
+      version=VERSION,
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      description=DESCRIPTION,
+      long_description=LONG_DESC,
+      license=LICENSE,
+      keywords=KEYWORDS,
+      packages=["dexml"],
+     )
+
