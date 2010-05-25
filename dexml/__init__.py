61dbfec5ad722a2cc6d4fbac7a8d25c259a861cf
Ryan Kelly <ryan@rfk.id.au>
2010-05-25 20:41:45 +1000

    prep version 0.3.6

--- dexml/__init__.py
+++ dexml/__init__.py
@@ -64,7 +64,7 @@ classes for more details:
 
 __ver_major__ = 0
 __ver_minor__ = 3
-__ver_patch__ = 5
+__ver_patch__ = 6
 __ver_sub__ = ""
 __version__ = "%d.%d.%d%s" % (__ver_major__,__ver_minor__,__ver_patch__,__ver_sub__)
                               
