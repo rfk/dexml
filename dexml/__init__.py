98db8c585187e4220c79bd175320d0a411edff46
Ryan Kelly <ryan@rfk.id.au>
2012-10-26 08:38:51 +1100

    Fix mysterious binary content in setup.py

--- dexml/__init__.py
+++ dexml/__init__.py
@@ -65,7 +65,7 @@ classes for more details:
 
 __ver_major__ = 0
 __ver_minor__ = 5
-__ver_patch__ = 0
+__ver_patch__ = 1
 __ver_sub__ = ""
 __version__ = "%d.%d.%d%s" % (__ver_major__,__ver_minor__,__ver_patch__,__ver_sub__)
                               
